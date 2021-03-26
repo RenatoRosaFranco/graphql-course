1da9114f09b71ede8af5a033ed2831c882fdbf96
Renato Rosa Franco <renato_ny@live.com>
2021-03-26 00:58:58 -0300

    Desafio do produto - ongoing.

--- index.js
+++ index.js
@@ -2,20 +2,68 @@ const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
 	scalar Date
-	
+
+	type Usuario {
+		id: ID!
+		nome: String!
+		email: String!
+		idade: Int
+		salario: Float
+		vip: Boolean
+	}
+
+	type Produto {
+		nome: String!
+		preco: Float!
+		desconto: Int
+	}
+
 	type Query {
+		id: Int
 		ola: String
 		horaAtual: Date
+		usuarioLogado: Usuario
+		produtoEmDestaque: Produto
 	}
 `
 
 const resolvers = {
+	Usuario: {
+		salario(usuario) {
+			return usuario.salario_real;
+		}
+	},
+	Produto: {
+		desconto(produto) {
+			return produto.preco - produto.desconto;
+		}
+	},
 	Query: {
+		id() {
+			return Math.floor(Math.random() * 100);
+		},
 		ola() {
 			return 'Basta retornar uma string.';
 		},
 		horaAtual() {
 			return `${new Date}`;
+		},
+		usuarioLogado() {
+			return {
+				id: Math.floor(Math.random() * 100),
+				nome: 'Ana da Web',
+				email: 'anadaweb@email.com',
+				idade: 23,
+				salario_real: 1234.56,
+				vip: true
+			}
+		},
+		produtoEmDestaque() {
+			return {
+				nome: 'Livro GraphQL',
+				preco: 100,
+				desconto: 1
+			}
 		}
 	}
 }
